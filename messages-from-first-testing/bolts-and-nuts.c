ccb7fa1b05d976bc0b800d15656cb14d73254c53
includealex <mishin.aa@phystech.edu>
2021-12-18 03:14:35 +0300

    fixed amounts of bugs

--- messages-from-first-testing/bolts-and-nuts.c
+++ messages-from-first-testing/bolts-and-nuts.c
@@ -7,119 +7,155 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define MSGSZ 1
+
 extern int errno;
 
 struct msgbuf_imitator {
   long mtype;
-  char mtext[1];
+  char mtext[MSGSZ];
 };
 
-int create_plan(int pl_id) {
-  errno = 0;
+void create_plan(int trinketInit, int bolt, int nut) {
+  struct msgbuf_imitator msgInit = {1, {0}};
 
-  struct msgbuf_imitator nut_msg = {1, {1}};
-  struct msgbuf_imitator bolt_msg = {2, {1}};
-  struct msgbuf_imitator not_done_msg = {3, {0}};
-  struct msgbuf_imitator done_msg = {3, {1}};
+  msgsnd(trinketInit, &msgInit, MSGSZ, 0);
 
-  msgsnd(pl_id, &nut_msg, 1, 0);
-  msgsnd(pl_id, &nut_msg, 1, 0);
-  msgsnd(pl_id, &bolt_msg, 1, 0);
-  msgsnd(pl_id, &not_done_msg, 1, 0);
-  msgsnd(pl_id, &not_done_msg, 1, 0);
-  msgsnd(pl_id, &done_msg, 1, 0);
-  if (errno != 0) {
-    return -1;
-  }
+  struct msgbuf_imitator msgInitWorker = {1, {1}};
 
-  return 0;
+  msgsnd(nut, &msgInitWorker, MSGSZ, 0);
+  msgsnd(nut, &msgInitWorker, MSGSZ, 0);
+  msgsnd(bolt, &msgInitWorker, MSGSZ, 0);
 }
 
-int boltter(int pl_id) {
-  struct msgbuf_imitator msg;
-  struct msgbuf_imitator nuts_done = {1, {0}};
-  int bolt_init = 0;
-  printf("Putted bolts\n");
+int nutter(int trinketInit, int bolt, int nut, int workPlan) {
   for (;;) {
-    if (!bolt_init) {
-      printf("Got a bolt\n");
-      ++bolt_init;
+    int val = 0;
+    struct msgbuf_imitator msg;
+
+    msgrcv(nut, &msg, MSGSZ, 1, 0);
+    msgrcv(workPlan, &msg, MSGSZ, 1, 0);
+
+    val = msg.mtext[0];
+    msgsnd(workPlan, &msg, MSGSZ, 0);
+
+    if (!val) {
+      return 0;
     }
-    msgrcv(pl_id, &msg, 1, 2, 0);
-    if (*msg.mtext) {
-      printf("Added a bolt\n");
-      --bolt_init;
-      msgrcv(pl_id, &msg, 1, 3, 0);
-      if (*msg.mtext) {
-        printf("Swapped the part\n");
-        msgsnd(pl_id, &nuts_done, 1, 0);
-        msgsnd(pl_id, &nuts_done, 1, 0);
-        create_plan(pl_id);
-      }
-    }
-    if (errno != 0) {
-      return -1;
+
+    printf("nut [%d] in work\n", getpid());
+
+    msgrcv(trinketInit, &msg, MSGSZ, 1, 0);
+    val = msg.mtext[0];
+
+    if (val == 2) {
+      msgrcv(workPlan, &msg, MSGSZ, 1, 0);
+
+      val = --msg.mtext[0];
+      msgsnd(workPlan, &msg, MSGSZ, 0);
+
+      printf("nut [%d] cought a detail\n", getpid());
+      printf("nut [%d] is done\n\n", getpid());
+
+      create_plan(trinketInit, bolt, nut);
+    } else {
+      struct msgbuf_imitator msgForSend = {1, {val + 1}};
+
+      msgsnd(trinketInit, &msgForSend, 1, 0);
+      printf("nut [%d] is done\n", getpid());
     }
   }
 }
 
-int nutter(int pl_id) {
-  struct msgbuf_imitator msg;
-  struct msgbuf_imitator bolts_done = {2, {0}};
-  struct msgbuf_imitator nuts_done = {1, {0}};
-  int nut_init = 0;
-  printf("Putting_nuts\n");
+int boltter(int trinketInit, int bolt, int nut, int workPlan) {
   for (;;) {
-    if (!nut_init) {
-      printf("Received a nut\n");
-      ++nut_init;
+    int val = 0;
+    struct msgbuf_imitator msg;
+
+    msgrcv(bolt, &msg, MSGSZ, 1, 0);
+    msgrcv(workPlan, &msg, MSGSZ, 1, 0);
+
+    val = msg.mtext[0];
+    msgsnd(workPlan, &msg, MSGSZ, 0);
+
+    if (!val) {
+      return 0;
     }
-    msgrcv(pl_id, &msg, 1, 1, 0);
-    if (*msg.mtext) {
-      printf("Putted a nut\n");
-      --nut_init;
-      msgrcv(pl_id, &msg, 1, 3, 0);
-      if (*msg.mtext) {
-        msgsnd(pl_id, &bolts_done, 1, 0);
-        msgsnd(pl_id, &nuts_done, 1, 0);
-        create_plan(pl_id);
-      }
+
+    printf("bolt [%d] in work\n", getpid());
+
+    msgrcv(trinketInit, &msg, MSGSZ, 1, 0);
+    val = msg.mtext[0];
+
+    if (val == 2) {
+      msgrcv(workPlan, &msg, MSGSZ, 1, 0);
+
+      --msg.mtext[0];
+      msgsnd(workPlan, &msg, MSGSZ, 0);
+
+      printf("bolt [%d] cought a detail\n", getpid());
+      printf("bolt [%d] is done\n\n", getpid());
+
+      create_plan(trinketInit, bolt, nut);
+    } else {
+      struct msgbuf_imitator msgForSend = {1, {val + 1}};
+
+      msgsnd(trinketInit, &msgForSend, MSGSZ, 0);
+      printf("bolt [%d] is done\n", getpid());
     }
-  }
-  if (errno != 0) {
-    return -1;
   }
 }
 
-int main() {
-  int pl_id = msgget(IPC_PRIVATE, IPC_CREAT | 0660);
+int main(int argc, char* argv[]) {
+  int ntrinkets = 1e6;
 
-  create_plan(pl_id);
+  if (argc == 2) {
+    ntrinkets = atoi(argv[1]);
+  }
+
+  if (argc > 2) {
+    printf("Incorrect number of arguments\n");
+    return 0;
+  }
+
+  int workPlan = msgget(IPC_PRIVATE, IPC_CREAT | 0660);
+  struct msgbuf_imitator msgInit = {1, {ntrinkets}};
+
+  msgsnd(workPlan, &msgInit, MSGSZ, 0);
+
+  int trinketInit = msgget(IPC_PRIVATE, IPC_CREAT | 0660);
+  int bolt = msgget(IPC_PRIVATE, IPC_CREAT | 0660);
+  int nut = msgget(IPC_PRIVATE, IPC_CREAT | 0660);
+
+  create_plan(trinketInit, bolt, nut);
 
   int pid = fork();
   if (!pid) {
-    nutter(pl_id);
+    nutter(trinketInit, bolt, nut, workPlan);
     return 0;
   }
+
   pid = fork();
   if (!pid) {
-    nutter(pl_id);
+    nutter(trinketInit, bolt, nut, workPlan);
     return 0;
   }
+
   pid = fork();
   if (!pid) {
-    boltter(pl_id);
+    boltter(trinketInit, bolt, nut, workPlan);
     return 0;
   }
 
-  usleep(1);
-
-  printf("Everything's done. We have created an art-looking like detail!!!\n");
-
-  msgctl(pl_id, IPC_RMID, NULL);
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; ++i) {
     wait(NULL);
   }
 
+  msgctl(trinketInit, IPC_RMID, NULL);
+  msgctl(bolt, IPC_RMID, NULL);
+  msgctl(nut, IPC_RMID, NULL);
+  msgctl(workPlan, IPC_RMID, NULL);
+
+  printf("!!!No way, it really works!!!\n");
   return 0;
 }
